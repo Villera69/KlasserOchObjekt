cf620a2e5af0b604ba2dbe25afc537ef8e408efb
Villera69 <william.ramsfeldt@gmail.com>
2022-11-30 14:29:29 +0100

    a part of errorhandling

--- KlasserOchObjekt/Program.cs
+++ KlasserOchObjekt/Program.cs
@@ -80,31 +80,114 @@ static class Program {
         MainMenu(ref bank);
 }
 
+static void MainMenu(ref Bank bank){
+        double totalBalance = 0;
+
+        for(int a = 0; a < bank.accounts.Count; a++){
+            totalBalance += bank.accounts[a].GetBalance();
+        }
+
+        Console.Clear();
+        Console.WriteLine("Currently signed up customers: " + bank.customers.Count + "\nCurrent revenue of the bank: " + totalBalance + "\n");
+        Console.WriteLine("Please choose what you want to do by pressing a number and then pressing enter.");
+        Console.WriteLine("(1) Sign up a new customer");
+        Console.WriteLine("(2) Sign up a new account");
+        Console.WriteLine("(3) Connect an account and a customer");
+        Console.WriteLine("(4) Show customer and connected account");
+        Console.WriteLine("(5) Change the balance of an account");
+        Console.WriteLine("(6) Close the program");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                    SignCustomer(ref bank);
+                break;
+            case "2":
+                    SignAccount(ref bank);
+                break;
+            case "3":
+                    ConnectAccAndCustomer(ref bank);
+                break;
+            case "4":
+                    ShowCustomer(ref bank);
+                break;
+            case "5":
+                    ChangeAccBalance(ref bank);
+                break;
+            case "6":
+                break;
+            default:
+                break;
+        }
+
+}
+
 static void SignCustomer(ref Bank bank){
     string firstName;
     string lastName;
     string id;
+    bool isOnlyNumbers = true;
     int accountNumber = -1;
-    Console.Clear();
-    Console.Write("Please enter the customers first name: ");
-    firstName = Console.ReadLine();
-
-    Console.Write("Please enter the customers last name: ");
-    lastName = Console.ReadLine();
-
-    Console.Write("Please enter the customers 12 digit social security number: ");
-    id = Console.ReadLine();
+    while (true){
+        Console.Clear();
+        Console.Write("Please enter the customers first name: ");
+        firstName = Console.ReadLine();
+        if (firstName.Length != 0)
+        {
+            break;
+        }
+        else{
+            Console.WriteLine("You need to type in a name with at least 1 character. Press enter to try again");
+            Console.ReadLine();
+        }
+    }
+    while (true){
+        Console.Clear();
+        Console.Write("Please enter the customers last name: ");
+        lastName = Console.ReadLine();
+        if (lastName.Length != 0)
+        {
+            break;
+        }
+        else{
+            Console.WriteLine("You need to type in a name with at least 1 character. Press enter to try again");
+            Console.ReadLine();
+        }
+    }
+    while(true){
+        Console.Clear();
+        Console.Write("Please enter the customers 12 digit social security number: ");
+        id = Console.ReadLine();
+        isOnlyNumbers = int.TryParse(id, out _);
+        if(id.Length != 12 && isOnlyNumbers){
+            Console.Clear();
+            Console.WriteLine("Hey! You need to type in a social security number with 12 digits! Press enter to try again");
+        }
+        else{
+            break;
+        }
+    }
+    accountNumber++;
     bank.customers.Add(new Customer(firstName, lastName, id, accountNumber));
-
     MainMenu(ref bank);
 }
 
 static void SignAccount(ref Bank bank){
     string ownerID;
-    Console.Clear();
-    Console.Write("To create an account, please enter the customers 12 digit social security number: ");
-    ownerID = Console.ReadLine();
-
+    bool isOnlyNumbers;
+   while(true){
+        Console.Clear();
+        Console.Write("To create an account, please enter the customers 12 digit social security number: ");
+        ownerID = Console.ReadLine();
+        isOnlyNumbers = int.TryParse(ownerID, out _);
+        if(ownerID.Length != 12 && isOnlyNumbers){
+            Console.Clear();
+            Console.WriteLine("Hey! You need to type in a social security number with 12 digits! Press enter to try again");
+        }
+        else{
+            break;
+        }
+    }
     bank.accounts.Add(new Account(ownerID, 0.00, 0));
     MainMenu(ref bank);
 }
@@ -161,47 +244,7 @@ static void ChangeAccBalance(ref Bank bank){
     MainMenu(ref bank);
 }
 
-static void MainMenu(ref Bank bank){
-        double totalBalance = 0;
 
-        for(int a = 0; a < bank.accounts.Count; a++){
-            totalBalance += bank.accounts[a].GetBalance();
-        }
-
-        Console.Clear();
-        Console.WriteLine("Currently signed up customers: " + bank.customers.Count + "\nCurrent revenue of the bank: " + totalBalance + "\n");
-        Console.WriteLine("Please choose what you want to do by pressing a number and then pressing enter.");
-        Console.WriteLine("(1) Sign up a new customer");
-        Console.WriteLine("(2) Sign up a new account");
-        Console.WriteLine("(3) Connect an account and a customer");
-        Console.WriteLine("(4) Show customer and connected account");
-        Console.WriteLine("(5) Change the balance of an account");
-        Console.WriteLine("(6) Close the program");
-
-        switch (Console.ReadLine())
-        {
-            case "1":
-                    SignCustomer(ref bank);
-                break;
-            case "2":
-                    SignAccount(ref bank);
-                break;
-            case "3":
-                    ConnectAccAndCustomer(ref bank);
-                break;
-            case "4":
-                    ShowCustomer(ref bank);
-                break;
-            case "5":
-                ChangeAccBalance(ref bank);
-                break;
-            case "6":
-                break;
-            default:
-                break;
-        }
-
-}
 }
 }
 
